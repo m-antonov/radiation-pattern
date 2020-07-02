15555617d77466ba9268dd834b0a74d6cebcd472
Mikhail Antonov <amo.mikhail@gmail.com>
2020-07-02 11:30:14 +0300

    feat: Pattern generator phi property/setter

--- pattern.py
+++ pattern.py
@@ -11,24 +11,32 @@ class RadiationPattern:
         self._lambda = self._c / self._f
         self._k = 2 * (np.pi / self._lambda)
         self._len = 2 * self._lambda
-        self._phi_cap = 35 * (np.pi / 180)
+        self._phi_raw = 35
+
+    def _phi_eval(self):
+        return self._phi_raw * (np.pi / 180)
 
     def _f0(self, theta, phi):
-        _left_side = self._k * self._len * (1 - np.sin(theta) * np.sin(self._phi_cap + phi))
-        _right_side = self._k * self._len * (1 - np.sin(theta) * np.sin(self._phi_cap - phi))
+        _left_side = self._k * self._len * (1 - np.sin(theta) * np.sin(self._phi_eval() + phi))
+        _right_side = self._k * self._len * (1 - np.sin(theta) * np.sin(self._phi_eval() - phi))
         return np.sinc(_left_side / 2) * np.sinc(_right_side / 2)
 
     def _f1(self, theta, phi):
         return np.power(np.cos(theta) * np.sin(phi) * self._f0(theta, phi), 2)
 
     def _f2(self, theta, phi):
-        return np.power((np.cos(phi) - np.sin(theta) * np.sin(self._phi_cap)) * self._f0(theta, phi), 2)
+        return np.power((np.cos(phi) - np.sin(theta) * np.sin(self._phi_eval())) * self._f0(theta, phi), 2)
 
     def _f3(self, theta, phi):
         return self._f1(theta, phi) + self._f2(theta, phi)
 
-    def set_phi(self, value):
-        self._phi_cap = value * (np.pi / 180)
+    @property
+    def phi(self):
+        return self._phi_raw
+
+    @phi.setter
+    def phi(self, value):
+        self._phi_raw = value
 
     def get_theta_r(self):
         _theta = np.arange(-np.pi, np.pi, 0.01)
